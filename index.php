b4e38ba3949c3598283055835ff88825b5ffdc90
bonampa312 <bonampa312@gmail.com>
2017-09-30 08:40:42 -0500

    Main page php heroku dojo

---
+++ index.php
@@ -0,0 +1,13 @@
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>This is a Heroku test app</title>
+  </head>
+  <body>
+    <?php
+      echo "Hello App Empresariales - Santiago Romero";
+    ?>
+  </body>
+</html>
